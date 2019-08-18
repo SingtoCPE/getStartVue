6a35461ba687570f3d35ab86fbdc2da2ae56a178
thitipat <thitipat.cpe18@hotmail.com>
2019-08-18 20:35:34 +0700

    initial Project

---
+++ employeegetstart/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-table :employees="employees" />
+  </div>
+</template>
+
+<script>
+import EmployeeTable from "@/components/EmployeeTable.vue";
+
+export default {
+  name: "app",
+  components: {
+    EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com"
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com"
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
+}
+</style>
---
+++ employeegetstart/src/components/EmployeeTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <!-- ...thead... -->
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: 'employee-table',
+  props: {
+    employees: Array,
+  },
+}
+</script>
+
+<style scoped></style>
